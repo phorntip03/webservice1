be46655b8109e22d892a9d8757dc6a19a95b8fe4
jaoh <oopontip7524@gmail.com>
2024-08-06 17:14:53 +0700

    up

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -8,5 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Country extends Model
 {
     use HasFactory;
-    
+    //1:m
+    public function region(){
+                                             //fk         pk
+        return $this->hasMany(Region::class,'country_id','id');
+    }
 }
