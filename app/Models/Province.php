70ffc170c2f336ee80c718904167f3a9b4e3ec6f
jaoh <oopontip7524@gmail.com>
2024-08-06 14:53:20 +0700

    up

--- app/Models/Province.php
+++ app/Models/Province.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Province extends Model
 {
     use HasFactory;
+    //1 to many 1:m
+    public function student(){
+        return $this->hasMany(Student::class,'province_id','id');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    //คำสั่งในการ debug ข้อมูล
+    //dd(55555);
+    //การ query ข้อมูล
+    //ดึงข้อมูลตารางทั้งหมดและเก็บค่าในตัวแปล model
+    $model = \App\Models\Province::all();
+    dd($model[0]->student[0]->name);
+
     return view('welcome');
 });
